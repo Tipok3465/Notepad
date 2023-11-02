3bd66b58d2e9f29be81a52860a4b1985d6e5b14b
Noname <108078194+Tipok3465@users.noreply.github.com>
2023-11-02 11:31:02 +0300

    Create header.h

---
+++ header.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <QLabel>
+#include <QPlainTextEdit>
+#include <QTextEdit>
+#include <QRegularExpression>
+#include <unordered_set>
+#include <QString>
+#include <string>
+#include <QWidget>
+#include <vector>
+#include <QAbstractButton>
+#include <QPushButton>
+#include <QCoreApplication>
+#include <QGuiApplication>
+#include <QApplication>
+#include <QMainWindow>
+#include <iostream>
+#include <fstream>
+#include <QFileDialog>
+#include <QTextBrowser>
